e22577ed46d66c92e24b03320b7ae2ee5211e7d4
Emirhan Yagci <emirhan.yacis@gmail.com>
2023-09-09 00:57:43 +0300

    container breakpoints added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import StarBackground from "./layouts/StarBackground.js";
 import GlobalStyles from "./styles/GlobalStyles.js";
+import ResetStyles from "./styles/ResetStyle.js";
 import "./styles/fontStyles.css";
 import styled from "styled-components";
-const StyledApp = styled.div``;
 const Container = styled.div`
-  width: 740px;
   margin: 0 auto;
+  @media only screen and (min-width: 640px) {
+    width: 100%;
+  }
+  @media only screen and (min-width: 768px) {
+    width: 90%;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 768px;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 1024px;
+  }
+  @media only screen and (min-width: 1536px) {
+    width: 1280px;
+  }
 `;
-const LayoutMain = styled.main`
-  background-color: #1d1d2b;
-`;
+
 const LayoutItem = styled.section`
   position: relative;
   z-index: 200;
@@ -20,15 +32,14 @@ function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
+      <ResetStyles />
+      <>
         <StarBackground />
-        <Container>
-          <LayoutMain>
-            <LayoutItem>aAAAAAAAAA</LayoutItem>
-            <LayoutItem>BBBBBBBB</LayoutItem>
-          </LayoutMain>
+        <Container className="appContainer">
+          <LayoutItem>aAAAAAAAAA</LayoutItem>
+          <LayoutItem>BBBBBBBB</LayoutItem>
         </Container>
-      </StyledApp>
+      </>
     </>
   );
 }
