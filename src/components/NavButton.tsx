44eadd14f74f15dc8e2faff58ef74f0bb0ba11a4
Emirhan Yagci <emirhan.yacis@gmail.com>
2023-09-10 23:35:22 +0300

    some jsx files converted to tsx

---
+++ src/components/NavButton.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+interface NavButtonProps {
+  href: string;
+  children: string;
+}
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  color: white;
+  font-size: 1.1rem;
+  padding: 0.9rem 1rem;
+  border-radius: 8px;
+  transition: all 300ms;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--color-brand-blue-2);
+  }
+`;
+export default function NavButton({ href, children }: NavButtonProps) {
+  return (
+    <a href={href}>
+      <Button>{children}</Button>
+    </a>
+  );
+}
---
+++ src/components/NavSocial.tsx
@@ -0,0 +1,35 @@
+import { TfiLinkedin } from "react-icons/tfi";
+import { BsGithub } from "react-icons/bs";
+import styled from "styled-components";
+
+const StyledNavSocial = styled.div`
+  text-align: end;
+`;
+const SocialIcons = styled.div`
+  display: inline-flex;
+  gap: 1rem;
+  padding: 0.5rem 1rem;
+  border-radius: 8px;
+  background-color: white;
+  cursor: pointer;
+  & > i {
+    transition: 200ms all;
+  }
+  & > i:hover {
+    transform: scale(1.2);
+  }
+`;
+export default function NavSocial() {
+  return (
+    <StyledNavSocial>
+      <SocialIcons>
+        <i>
+          <TfiLinkedin size="1.5rem" />
+        </i>
+        <i>
+          <BsGithub size="1.5rem" />
+        </i>
+      </SocialIcons>
+    </StyledNavSocial>
+  );
+}
