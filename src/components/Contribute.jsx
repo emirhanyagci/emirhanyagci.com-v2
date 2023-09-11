6eb56fc30ba5c3798f6eb0f0d9e875962cfc833c
Emirhan Yagci <emirhan.yacis@gmail.com>
2023-09-11 23:14:50 +0300

    user name added above of contribute table

--- src/components/Contribute.jsx
+++ src/components/Contribute.jsx
@@ -2,9 +2,20 @@ import React from "react";
 import styled from "styled-components";
 const StyledContribute = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
   justify-content: center;
   align-items: center;
   height: 250px;
+  & > a {
+    color: white;
+    font-size: 1.2rem;
+    font-weight: 300;
+    cursor: pointer;
+    &:hover {
+      font-weight: 400;
+    }
+  }
 `;
 const ContributeImg = styled.img`
   width: 70%;
@@ -13,6 +24,7 @@ const ContributeImg = styled.img`
 export default function Contribute() {
   return (
     <StyledContribute>
+      <a>@emirhanyagci</a>
       <ContributeImg
         src="https://ghchart.rshah.org/D1A124/emirhanyagci"
         alt="2016rshah's Blue Github Chart"
